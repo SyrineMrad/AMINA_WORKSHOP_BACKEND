a5f4cfadb770e1268210a8a18e4aee008ede0c40
syrinemrad <syrine.mrad96@hotmail.com>
2020-07-25 21:24:22 +0200

    papier crud

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- src/Controller/Api/PapierController.php
+++ src/Controller/Api/PapierController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/api")
@@ -21,11 +22,12 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 class PapierController extends AbstractController
 {
   /**
-     * @Route("/papier", name="add_papier", methods={"POST"})
+     * @Route("/add_papier", name="add_papier", methods={"POST"})
      * @IsGranted("ROLE_ADMIN")
      */
-    public function new(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    public function new(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator,Security $security)
     {
+        $user = $security->getUser();// A faire : enregister user_id avec le papier
         $papier = $serializer->deserialize($request->getContent(), Papier::class, 'json');
         $errors = $validator->validate($papier);
         if(count($errors)) {
@@ -34,7 +36,7 @@ class PapierController extends AbstractController
                 'Content-Type' => 'application/json'
             ]);
         }
-        $entityManager->persist($phone);
+        $entityManager->persist($papier);
         $entityManager->flush();
         $data = [
             'status' => 201,
@@ -42,4 +44,67 @@ class PapierController extends AbstractController
         ];
         return new JsonResponse($data, 201);
     }
+    /**
+     * @Route("/papiers", name="list_papier", methods={"GET"})
+     */
+    public function getSendedPapiers(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer)
+    {
+        $papiers = $entityManager->getRepository(Papier::class)->findBy(array('etat' => true));
+        $data = $serializer->serialize($papiers, 'json');
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/draftpapiers", name="listdraft_papier", methods={"GET"})
+     */
+    public function getDraftPapiers(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer)
+    {
+        $papiers = $entityManager->getRepository(Papier::class)->findBy(array('etat' => false));
+        $data = $serializer->serialize($papiers, 'json');
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/update_papier/{id}", name="update_papier", methods={"PUT"})
+     */
+    public function update(Request $request, SerializerInterface $serializer, Papier $papier, ValidatorInterface $validator, EntityManagerInterface $entityManager)
+    {
+        $papierUpdate = $entityManager->getRepository(Papier::class)->find($papier->getId());
+        $data = json_decode($request->getContent());
+        foreach ($data as $key => $value){
+            if($key && !empty($value)) {
+                $name = ucfirst($key);
+                $setter = 'set'.$name;
+                $papierUpdate->$setter($value);
+            }
+        }
+        $errors = $validator->validate($papierUpdate);
+        if(count($errors)) {
+            $errors = $serializer->serialize($errors, 'json');
+            return new Response($errors, 500, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        $entityManager->flush();
+        $data = [
+            'status' => 200,
+            'message' => 'papier a bien été mis à jour'
+        ];
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/delete_papier/{id}", name="delete_papier", methods={"DELETE"})
+     */
+    public function delete(Papier $papier, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($papier);
+        $entityManager->flush();
+        return new Response(null, 204);
+    }
+
 }
